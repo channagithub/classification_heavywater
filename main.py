efcb45d3d3aa8c27df58de51b62fc7c47e7f6248
channagithub <sendmailtochanna@gmail.com>
2019-02-22 21:19:59 +0000

    linear svc model

--- main.py
+++ main.py
@@ -3,28 +3,34 @@ import os
 from flask import Flask
 from flask import request
 from flask import jsonify
-
-def split_data():
-    # split train test and varification data here
-    pass
-
-def load_data():
-    split_data()
-    # load data here
-    pass
-
-def train_model():
-    load_data()
-    # train model here
-    # store the model to filesystem
-    pass
+from joblib import load
 
 app = Flask(__name__)
+
+
+global tfidf, model, id_to_class
+
+tfidf = load('tfidf.pkl') 
+print("[DEBUG] loading tfidf done!", flush=True)
+model = load('model.pkl') 
+print("[DEBUG] loading model done!", flush=True)
+id_to_class = load('id_to_class.pkl') 
+print("[DEBUG] loading id_to_class done!", flush=True)
 
 @app.route('/isAlive', methods=['GET'])
 def index():
     return jsonify(return_value = "channa")
 
-if __name__ == '__main__':
+@app.route('/get_prediction', methods=['POST'])
+def _get_prediction():
 
-    train_model()
+    filters = request.get_json()
+    sentences_list = filters.get('sentences', dict())
+
+    vectorized_input = tfidf.transform(sentences_list).toarray()
+    predicted_values = [id_to_class[i] for i in model.predict(vectorized_input)]
+
+    return jsonify(ret_pred_values = predicted_values)
+
+if __name__ == "__main__":
+    print("Loading!")
