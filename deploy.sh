4697c1a2a33d5bc7f0a1533c949f1cdb35d08f9a
Channa <sendmailtochanna@gmail.com>
2019-02-22 21:26:20 -0700

    fix timeout issue for loading all trained models

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 docker-compose --file docker-compose.yml down -v --remove-orphans
-docker-compose --file docker-compose.yml up -d --build --force-recreate
+docker-compose --file docker-compose.yml up -d --timeout=180 --build --force-recreate
